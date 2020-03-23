81e4c016a7f4fd8594dbbf32285d4da42941d7ee
Paulo Matos <pmatos@linki.tools>
2020-03-23 10:31:06 +0100

    Initial config files for an ARM32 board

---
+++ board/qemu-arm32-jsc/post_build.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+# Allow users to ssh without password
+echo 'PermitEmptyPasswords yes' >> $1/etc/ssh/sshd_config
+
+# Allow jsc user to halt
+SUDOERS=$1/etc/sudoers
+chmod +w ${SUDOERS}
+echo 'jsc ALL=NOPASSWD: /sbin/halt, /sbin/reboot, /sbin/poweroff' >> ${SUDOERS}
+chmod -w ${SUDOERS}
+
