e5c9f38ffcf0429c424e47b582e9117dbf4431ac
Paulo Matos <pmatos@linki.tools>
2020-09-10 10:00:32 +0200

    Allow jsc to become superuser

--- board/qemu-mips32elr2-jsc/post_build.sh
+++ board/qemu-mips32elr2-jsc/post_build.sh
@@ -5,6 +5,6 @@ echo 'PermitEmptyPasswords yes' >> $1/etc/ssh/sshd_config
 # Allow jsc user to halt
 SUDOERS=$1/etc/sudoers
 chmod +w ${SUDOERS}
-echo 'jsc ALL=NOPASSWD: /sbin/halt, /sbin/reboot, /sbin/poweroff' >> ${SUDOERS}
+echo 'jsc ALL=NOPASSWD: /sbin/halt, /sbin/reboot, /sbin/poweroff, /bin/su' >> ${SUDOERS}
 chmod -w ${SUDOERS}
 
